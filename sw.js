3a28948811a38c3382460ba5b4e6157a9194ea94
holasoyender <63854611+holasoyender@users.noreply.github.com>
2021-06-13 18:55:51 +0200

    Simple test for PWA

---
+++ sw.js
@@ -0,0 +1,1 @@
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js");workbox.routing.registerRoute(({requests})=>request.destination==="image",new workbox.strategies.CacheFirst());
